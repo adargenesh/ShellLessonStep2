b131f3eb757ff56d94c96a9f58fd3b48ef8d9a21
User <User@LAB2-2>
2024-02-28 09:37:26 +0200

    view model

---
+++ ViewModels/BearsViewModel.cs
@@ -0,0 +1,48 @@
+﻿using ShellLessonStep2.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShellLessonStep2.Services;
+
+namespace ShellLessonStep2.ViewModels
+{
+    internal class BearsViewModel : ViewModelBase   
+    {
+        private ObservableCollection<Bears> bears;
+        public ObservableCollection<Bears> Bears
+        {
+            get
+            {
+                return this.bears;
+            }
+            set
+            {
+                this.bears = value;
+                OnPropertyChanged();
+
+
+            }
+        }
+
+
+        private AnimalService BearsService;
+        public BearsViewModel(AnimalService service)
+        {
+            this.studentsService = service;
+            students = new ObservableCollection<Student>();
+            ReadStudents();
+        }
+
+        private async void ReadStudents()
+        {
+            StudentsService service = this.studentsService;
+            List<Student> list = await service.GetStudents();
+            this.Students = new ObservableCollection<Student>(list);
+        }
+
+
+    }
+}
---
+++ ViewModels/MonkeysViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ShellLessonStep2.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellLessonStep2.ViewModels
+{
+    internal class MonkeysViewModel : ViewModelBase 
+    {
+        private ObservableCollection<Monkeys> monkeys;
+        public ObservableCollection<Monkeys> Monkeys
+        {
+            get
+            {
+                return this.monkeys;
+            }
+            set
+            {
+                this.monkeys = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
